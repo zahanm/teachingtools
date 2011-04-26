879e61cef0676917a305bc27a09ad78f312f5331
Zahan Malkani <zahanm@gmail.com>
2011-04-25 22:54:16 -0700

    Updated maze interface Embellished README

--- simplemaze/maze.py
+++ simplemaze/maze.py
@@ -1,20 +1,21 @@
 
 class Maze:
   """
-  
+  'Black box' used to hide details of graph used
+  Exercise is to use queues and BFS to find shortest path to exit
   """
   
   def __init__(self, layout_fname):
     with open(layout_fname, 'r') as layout_file:
       pass
   
-  def getNeighbors(self, node):
+  def get_neighbors(self, node):
     pass
   
-  def isExit(self, node):
+  def is_exit(self, node):
     pass
   
-  def haveVisited(self, node):
+  def have_visited(self, node):
     pass
 
 def test_maze():
